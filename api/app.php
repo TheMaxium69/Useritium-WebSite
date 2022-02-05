0d81f3568da781d615e7c1ce897efad7aebde263
TheMaximeSan <mxm.tournier@gmail.com>
2022-02-05 18:04:55 +0100

    update

--- api/app.php
+++ api/app.php
@@ -1,6 +1,13 @@
 <?php
 
-include "db.php";
-
+//DateBase
+require_once "private/db.php";
 $ConnectDB = mysqli_connect($host, $userDB, $passDB, $Database);
+
+//Salt
+require_once "private/salt.php";
+$key = $salt;
+
+session_start();
+
 
