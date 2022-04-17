dc4a4236c265000c230470316ee99eaf8c08bc88
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-04-18 00:14:01 +0200

    update

--- app/extension.php
+++ app/extension.php
@@ -2,5 +2,6 @@
 include "env.php";
 
 require_once $env_urlFile . "Boostrap.phtml";
+require_once $env_urlFile . "FontAwasome.phtml";
 require_once $env_urlFile . "iziToast.phtml";
 require_once $env_urlFile . "link.phtml";
