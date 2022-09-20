5acb1979b18f599c8c6d11b29d8f45d6e7b37981
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-09-20 21:43:07 +0200

    update graph

--- index.php
+++ index.php
@@ -2,13 +2,68 @@
 <body> <header> <?php navbar($page); ?> </header>
 
 <main>
-
-
-    <p>Page commercial</p>
-
-
+    
+    <section id="devise" class="container my-5">
+        <div class="row align-items-center">
+            <img class="col-3" src="assets/" alt="Lorem Ispu">
+            <article class="col-8 offset-1">
+                <h2 class="text-uppercase text-center fw-bold">Lorem Ispu</h2>
+                Consequat dolore dolore pariatur veniam nulla proident. Do ut aute dolor id reprehenderit ut aliquip qui.
+                Voluptate ullamco non minim et. Sit amet cillum non quis amet esse sint ad nulla commodo exercitation cillum
+                anim.
+            </article>
+        </div>  
+    </section>
+    <section id="fabrication">
+        <div class="container">
+            <h2>onsequat dolore dolore</h2>
+            <ul>
+                <li class="col align-self-center">
+                    <img src="assets/" alt="Lorem Ispu">
+                    <h3>onsequat dolore dolore pariatur veniam nulla proident. Do ut aute dolor</h3>
+                </li>
+                <li class="col align-self-center">
+                    <img src="assets/" alt="Lorem Ispu">
+                    <h3>onsequat dolore dolore pariatur veniam nulla proident. Do ut aute dolor</h3>
+                </li>
+                <li class="col align-self-center">
+                    <img src="assets/" alt="Lorem Ispu">
+                    <h3>onsequat dolore dolore pariatur veniam nulla proident. Do ut aute dolor</h3>
+                </li>
+                <li class="col align-self-center">
+                    <img src="assets/" alt="Lorem Ispu">
+                    <h3>onsequat dolore dolore pariatur veniam nulla proident. Do ut aute dolor</h3>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section id="items" class="container">
+        <h2>Lorem Ispu</h2>
+        <ul>
+            <li class="col align-self-center">
+                <img src="assets/" alt="Lorem Ispu">
+                <h3>Lorem Ispu</h3>
+                <p>Ipsum qui adipisicing quis mollit nostrud laboris dolore.</p>
+            </li>
+            <li class="col align-self-center">
+                <img src="assets/" alt="Lorem Ispu">
+                <h3>Lorem Ispu</h3>
+                <p>Ipsum qui adipisicing quis mollit nostrud laboris dolore.</p>
+            </li>
+            <li class="col align-self-center">
+                <img src="assets/" alt="Lorem Ispu">
+                <h3>Lorem Ispu</h3>
+                <p>Ipsum qui adipisicing quis mollit nostrud laboris dolore.</p>
+            </li>
+        </ul>
+    </section>
 
 </main>
+
+
+    <footer>
+        2022 © TYROLIUM
+    </footer>
 
 
 </body> </html>
