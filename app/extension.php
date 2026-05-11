e758145111f1fbcbb3d0c3078814681decaf5072
TheMaximeSan <mxm.tournier@gmail.com>
2026-05-11 22:09:25 +0200

    update

--- app/extension.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "env.php";
-
-require_once $env_urlFile . "Boostrap.phtml";
-require_once $env_urlFile . "FontAwasome.phtml";
-require_once $env_urlFile . "iziToast.phtml";
-require_once $env_urlFile . "link.phtml";
