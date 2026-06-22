92de3c391347e4ddf13adc019e7633c6209fe4a9
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-22 22:35:59 +0200

    add 403

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { TyroUiNotFound } from 'tyrolium-ui';
+import { TyroUiForbidden, TyroUiNotFound } from 'tyrolium-ui';
 
 export const routes: Routes = [
-    { path: '',   component: Home },
-    { path: '**', component: TyroUiNotFound },
+    { path: '',    component: Home },
+    { path: '403', component: TyroUiForbidden },
+    { path: '**',  component: TyroUiNotFound },
 ];
